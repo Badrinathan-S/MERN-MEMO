30e5d2b840d0e7998c9ccac14e5eaf63e55e46b7
-Badrinathan-S <badrinathan3@gmail.com>
2022-07-10 08:22:34 +0530

    register and login backend completed

---
+++ backend/controllers/userController.js
@@ -0,0 +1,57 @@
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already exist");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    pic,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occurred!!");
+  }
+  res.json({
+    name,
+    email,
+  });
+});
+
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      pic: user.pic,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user or email!!");
+  }
+});
+
+module.exports = { registerUser, authUser };
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,14 @@ const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/connetDB");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 dotenv.config();
 connectDB();
 const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("api running");
@@ -20,5 +23,10 @@ app.get("/api/notes/:id", (req, res) => {
   const note = notes.find((n) => n._id === req.params.id);
   res.send(note);
 });
+
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`server start at ${PORT}`));
