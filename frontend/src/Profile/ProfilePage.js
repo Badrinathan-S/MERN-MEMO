de3b638f8a2f4989e88e328febd1f6ba69fd2aee
-Badrinathan-S <badrinathan3@gmail.com>
2022-08-10 08:24:15 +0530

    Profile page frontend inprogress!!

--- frontend/src/Profile/ProfilePage.js
+++ frontend/src/Profile/ProfilePage.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import MainScreen from "../component/MainScreen";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+
+  const { userInfo } = userLogin;
+
+  const userUpdate = useSelector((state) => state.userUpdate);
+
+  const { loading, error, success } = userUpdate;
+
+  return (
+    <MainScreen title="EDIT PROFILE">
+      <div>
+        <Row className="profileContainer">
+          <Col md={6}>Form</Col>
+          <Col
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            Profilepic
+          </Col>
+        </Row>
+      </div>
+    </MainScreen>
+  );
 };
 
 export default ProfilePage;
