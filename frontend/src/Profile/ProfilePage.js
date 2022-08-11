39d8e006e40b2653c1e40e12af8afe7b356776b5
-Badrinathan-S <badrinathan3@gmail.com>
2022-08-11 08:33:22 +0530

    Profile Page UI inProgress!!

--- frontend/src/Profile/ProfilePage.js
+++ frontend/src/Profile/ProfilePage.js
@@ -1,9 +1,16 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import MainScreen from "../component/MainScreen";
 
 const ProfilePage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pic, setPic] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [picMessage, setPicMessage] = useState();
+
   const dispatch = useDispatch();
 
   const userLogin = useSelector((state) => state.userLogin);
@@ -14,11 +21,52 @@ const ProfilePage = () => {
 
   const { loading, error, success } = userUpdate;
 
+  const submitHandler = () => {};
+
   return (
     <MainScreen title="EDIT PROFILE">
       <div>
         <Row className="profileContainer">
-          <Col md={6}>Form</Col>
+          <Col md={6}>
+            <Form onSubmit={submitHandler}>
+              <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+              <Form.Group controlId="confirmPassword">
+                <Form.Label>Confirm Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                ></Form.Control>
+              </Form.Group>
+            </Form>
+          </Col>
           <Col
             style={{
               display: "flex",
